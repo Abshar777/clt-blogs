77368720f8a42c8934b28f1d6a84fbc5474f2048
Abshar777 <absharameen625@gmail.com>
2026-01-25 15:59:10 +0530

    feat: Add React Quill snow theme stylesheet to global layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,6 +64,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+          <link rel="stylesheet" href="https://unpkg.com/react-quill-new@1.0.2/dist/quill.snow.css"></link>
          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"></link>
       </head>
       <body className={`font-sans antialiased`}>
